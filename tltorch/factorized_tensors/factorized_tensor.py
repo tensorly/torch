76b30128e1216251b7756b01306ffe396912dd6f
Jean Kossaifi <jean.kossaifi@gmail.com>
2021-05-14 21:44:28 +0100

    FIX for TT

--- tltorch/factorized_tensors/factorized_tensor.py
+++ tltorch/factorized_tensors/factorized_tensor.py
@@ -339,7 +339,7 @@ class TTTensor(FactorizedTensor, name='TT'):
             factors = tensor_train(tensor, self.rank)
         
         self.factors = FactorList([nn.Parameter(f) for f in factors])
-        self.rank = [f.shape[0] for f in factors] + [1]
+        self.rank = tuple([f.shape[0] for f in factors] + [1])
         return self
 
     @property
@@ -425,7 +425,7 @@ class TTTensor(FactorizedTensor, name='TT'):
         # Important: don't increment the order before accessing factors which uses order!
         self.order += 1
         new_rank = self.rank[mode]
-        self.rank = self.rank[:mode] + [new_rank] + self.rank[mode:]
+        self.rank = self.rank[:mode] + (new_rank, ) + self.rank[mode:]
         self.shape = self.shape[:mode] + (new_dim, ) + self.shape[mode:]
 
         # Init so the reconstruction is equivalent to concatenating the previous self new_dim times
