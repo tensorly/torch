435a9bcb3964b855263f19911d7c8e7a33006518
Jean Kossaifi <jean.kossaifi@gmail.com>
2022-06-14 12:52:29 -0700

    FIX factorized linear

--- tltorch/functional/linear.py
+++ tltorch/functional/linear.py
@@ -2,7 +2,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from ..factorized_tensors import TensorizedTensor
+from ..factorized_tensors.tensorized_matrices import CPTensorized, TuckerTensorized, BlockTT
 from .factorized_linear import linear_blocktt, linear_cp, linear_tucker
+import pdb
 
 import tensorly as tl
 tl.set_backend('pytorch')
@@ -21,15 +23,17 @@ def factorized_linear(x, weight, bias=None, in_features=None):
         # Weights are in the form (out_features, in_features) 
         # PyTorch's linear returns dot(x, weight.T)!
         if isinstance(weight, TensorizedTensor):
-            if weight._factorization == 'cp':
-                return linear_cp(x, weight) + bias
-            elif weight._factorization == 'tucker':
-                return linear_tucker(x, weight) + bias
-            elif weight._factorization == 'blocktt':
-                return linear_blocktt(x, weight) + bias
+            x_shape = x.shape[:-1] + weight.tensorized_shape[0]
+            out_shape = x.shape[:-1] + (-1, )
+            if isinstance(weight, CPTensorized):
+                return linear_cp(x.reshape(x_shape), weight).reshape(out_shape) + bias
+            elif isinstance(weight, TuckerTensorized):
+                return linear_tucker(x.reshape(x_shape), weight).reshape(out_shape) + bias
+            elif isinstance(weight, BlockTT):
+                return linear_blocktt(x.reshape(x_shape), weight).reshape(out_shape) + bias
             # if no efficient implementation available: use reconstruction
             weight = weight.to_matrix()
         else:
             weight = weight.to_tensor()
-
+        
     return F.linear(x, torch.reshape(weight, (-1, in_features)), bias=bias)
